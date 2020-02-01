df71af39053ed46e15da8b53c21fbe5a6736f4c6
jinggu <guji050394@shms-mail.ch>
2020-01-31 23:08:05 -0500

    [ADD] v1

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // CALL THE OWL PLUGIN
+  $("#slider").owlCarousel({
+    nav: true,
+    items: 2,
+    autoplay: true,
+    loop: true,
+    smartSpeed: 2500
+  });
+});
