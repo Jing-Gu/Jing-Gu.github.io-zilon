0a90e72f5f25210fadd94e027dec8b38d6d928bf
jinggu <guji050394@shms-mail.ch>
2020-02-13 21:39:38 -0500

    + V3 :  define CSS variables, add contact, add Favicon, better button, changed the style of owl slider

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // CALL THE OWL PLUGIN
   $("#slider").owlCarousel({
     nav: true,
-    items: 2,
+    items: 1,
     autoplay: true,
     loop: true,
     smartSpeed: 2500
