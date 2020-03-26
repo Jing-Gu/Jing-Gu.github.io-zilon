a471343d47e6854251e8d100293b498c0196b45d
jinggu <guji050394@shms-mail.ch>
2020-03-26 15:15:17 -0400

    + AOS, + sectionObserver for header (changing styles), changed contact card style, changed button style, changed murals px to % vh

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  AOS.init();
+
   // CALL THE OWL PLUGIN
   $("#slider").owlCarousel({
     nav: true,
@@ -12,5 +14,40 @@ $(document).ready(function() {
   $("#burger").click(function() {
     $("nav").toggleClass("menu-active");
     $("#burger").toggleClass("burger-active");
+  });
+
+  //once click and go to each section, close the toggle menu by removing the class
+  $("#menu a").click(function() {
+    $("nav").removeClass("menu-active");
+    $("#burger").removeClass("burger-active");
+  });
+
+  //sectionObserver for header changing styles
+  const header = document.querySelector("header");
+  const section = document.getElementById("slider");
+
+  const sectionOptions = {
+    rootMargin: "-100px 0px 0px 0px"
+  };
+
+  const sectionObserver = new IntersectionObserver(
+    (entries, sectionObserver) => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          header.classList.add("header-scrolled");
+        } else {
+          header.classList.remove("header-scrolled");
+        }
+      });
+    },
+    sectionOptions
+  );
+
+  sectionObserver.observe(section);
+
+  //mouseover contact card
+  const contact = document.getElementById("contact");
+  contact.addEventListener("mouseover", () => {
+    contact.classList.add("animated", "swing");
   });
 });
